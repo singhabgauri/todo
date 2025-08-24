dedf5edc6890c3935d4abf92ce804305c4023abf
singhbgauri <gaurisingh5544@gmail.com>
2025-08-24 22:44:46 +0530

    first commit

--- src/main/java/first_project/first/MyfirstApplication.java
+++ src/main/java/first_project/first/MyfirstApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyfirstApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyfirstApplication.class, args);
+		try{
+		SpringApplication.run(MyfirstApplication.class, args);}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 
 }
